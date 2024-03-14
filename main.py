d91b0b996bca5bef4d65892f7239995b4dba5f33
Przemek B <pprzemek89@gmail.com>
2024-03-14 11:01:52 +0100

    test

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ ilosc_Parzystych = 0
 tyle_Samo = 0
 same_Zera = 0
 same_Jedynki = 0
-zakonczenie = 1
+zakonczenie = "1"
 with open("napisy.txt") as plik:
     for i in range(len(plik.readline())):
         linijka = plik.readline()
@@ -13,22 +13,23 @@ with open("napisy.txt") as plik:
         if linijka == plik.readline():
             tyle_Samo += 1
     # c.
-        if len(linijka) != 1:
-            same_Zera += 1
-        if len(linijka) != 0:
-            same_Jedynki += 1
+        for i in range(len(linijka)):
+            if len(linijka) != "1":
+                same_Zera += 1
+            if len(linijka) != 0:
+                same_Jedynki += 1
     # d.
         for i in range(len(linijka)):
-            if linijka[-1] == zakonczenie:
+            if linijka[-2] == zakonczenie:
                 print("konczy sie 1:" + linijka)
         #e.
-        od_Tylu = "".join(reversed(linijka))
-        if od_Tylu == linijka:
+        od_Tylu = "".join(reversed(plik.readline()))
+        if od_Tylu == plik.readline():
             print("palindrom:"+od_Tylu +" "+ linijka)
         #f.
         for k in range(0,17):
-            if k == len(linijka):
-                print("K rowne: "+str(k-1)+" "+linijka)
+            if k == len(linijka)-1:
+                print("K rowne: "+str(k)+" "+linijka)
 
     print("tyle jest parzystych:"+str(ilosc_Parzystych))
     print("tyle jest napisow takich takich samych:"+str(tyle_Samo))
